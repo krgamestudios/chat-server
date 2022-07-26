fb4d857224371dfde5b57a503555811b064f4abf
Kayne Ruse <kayneruse@gmail.com>
2022-07-26 13:40:18 +0100

    Tweaked cors handing

--- server/server.js
+++ server/server.js
@@ -7,14 +7,19 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server, {
 	cors: {
-		origin: '*'
+		origin: process.env.WEB_ORIGIN
 	}
 });
 const cors = require('cors');
 
 //config
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+	credentials: true,
+	origin: [`${process.env.WEB_ORIGIN}`], //because auth-server
+	allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization', 'Set-Cookie'],
+	exposedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization', 'Set-Cookie'],
+}));
 
 //database connection
 const database = require('./database');
