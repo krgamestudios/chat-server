2e8c3f98cac1131bcd390e9e86ccea38ec43ed41
Ratstail91 <kayneruse@gmail.com>
2024-05-03 09:27:29 +1000

    Fully tested the remote database
    Updated docker base image

--- tools/debug-startup.sql
+++ tools/debug-startup.sql
@@ -1,4 +1,4 @@
 #use this while debugging
-CREATE DATABASE IF NOT EXISTS chat;
-CREATE USER IF NOT EXISTS 'chat'@'%' IDENTIFIED BY 'blastoise';
+CREATE DATABASE chat;
+CREATE USER 'chat'@'%' IDENTIFIED BY 'blastoise';
 GRANT ALL PRIVILEGES ON chat.* TO 'chat'@'%';
